3be11e07efa3c1d2e9206de1ca296ad72189ab05
Lucas Silveira <85209669+LucasSquariz@users.noreply.github.com>
2023-06-25 19:21:22 -0300

    ;sparkles: Feat: Item manager

--- Assets/Scripts/Items/ItemCollectableCoin.cs
+++ Assets/Scripts/Items/ItemCollectableCoin.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Itens;
 
 public class ItemCollectableCoin : ItemCollectableBase
 {
@@ -8,7 +9,7 @@ public class ItemCollectableCoin : ItemCollectableBase
     protected override void OnCollect()
     {
         base.OnCollect();
-        ItemManager.Instance.AddCoins();
+        ItemManager.Instance.AddByType(ItemType.COIN);
         coinCollider.enabled = false;
     }
 }
--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -3,41 +3,77 @@ using System.Collections.Generic;
 using UnityEngine;
 using Ebac.Core.Singleton;
 using TMPro;
+using NaughtyAttributes;
 
-public class ItemManager : Singleton<ItemManager>
+namespace Itens
 {
-    public SOInt coins;
-    public SOInt artifacts;
-    public TextMeshProUGUI uiTextCoins;
-    public TextMeshProUGUI uiTextArtifacts;
-
-    private void UpdateUI()
+    public enum ItemType
     {
-        //UIInGameManager.UpdateTextCoins(coins.ToString());
-        //UIInGameManager.UpdateTextArtifacts(artifacts.ToString());
-        // uiTextCoins.text = coins.ToString();
+        COIN,
+        LIFE_PACK
     }
 
-    private void Start()
+    public class ItemManager : Singleton<ItemManager>
     {
-        Reset();
+        public List<ItemSetup> itemSetups;        
+        
+        public TextMeshProUGUI uiTextCoins;
+        public TextMeshProUGUI uiTextArtifacts;
+
+        private void UpdateUI()
+        {
+            //UIInGameManager.UpdateTextCoins(coins.ToString());
+            //UIInGameManager.UpdateTextArtifacts(artifacts.ToString());
+            // uiTextCoins.text = coins.ToString();
+        }
+
+        private void Start()
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            foreach (var item in itemSetups)
+            {
+                item.soInt.value = 0;
+            }
+        }
+
+        public void AddByType(ItemType itemType, int amount = 1)
+        {
+            if (amount < 0) return;
+            itemSetups.Find(i => i.itemType == itemType).soInt.value += amount;            
+            UpdateUI();
+        }
+
+        public void RemoveByType(ItemType itemType, int amount = 1)
+        {
+            if (amount > 0) return;
+            var item = itemSetups.Find(i => i.itemType == itemType).soInt.value;
+            item -= amount;
+
+            if(item < 0) item = 0;
+        }
+
+        [Button]
+        private void AddCoin()
+        {
+            AddByType(ItemType.COIN);
+        }
+
+        [Button]
+        private void AddLifePack()
+        {
+            AddByType(ItemType.LIFE_PACK);
+        }
+
     }
 
-    private void Reset()
+    [System.Serializable]
+    public class ItemSetup
     {
-        coins.value = 0;
-        artifacts.value = 0;
-    }
-
-    public void AddCoins(int amout = 1)
-    {
-        coins.value += amout;
-        UpdateUI();
-    }
-
-    public void AddArtifacts(int amout = 1)
-    {
-        artifacts.value += amout;
-        UpdateUI();
+        public ItemType itemType;
+        public SOInt soInt;
     }
 }
