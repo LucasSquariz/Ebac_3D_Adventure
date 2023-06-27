fd4a25949a9bb783b3f518064928600d985b41f7
Lucas Silveira <85209669+LucasSquariz@users.noreply.github.com>
2023-06-27 18:21:35 -0300

    :sparkles: Feat: Cloth manager

---
+++ Assets/Scripts/Cloth/ClothManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ebac.Core.Singleton;
+
+namespace Cloth
+{
+    public enum ClothType
+    {
+        SPEED,
+    }
+
+    public class ClothManager : Singleton<ClothManager>
+    {
+        public List<ClothSetup> clothSetups;
+
+        public ClothSetup GetSetupByType(ClothType type)
+        {
+            return clothSetups.Find(i => i.clothType == type);
+        }
+    }
+
+    [System.Serializable]
+    public class ClothSetup
+    {
+        public ClothType clothType;
+        public Texture2D texture;
+    }
+}
+
