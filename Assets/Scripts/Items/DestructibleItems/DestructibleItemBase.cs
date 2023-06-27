c491fa606ec6b8c478cf6d3f40f28a84720ed91d
Lucas Silveira <85209669+LucasSquariz@users.noreply.github.com>
2023-06-26 21:40:25 -0300

    :sparkles: Feat: Drop from destructible objects

--- Assets/Scripts/Items/DestructibleItems/DestructibleItemBase.cs
+++ Assets/Scripts/Items/DestructibleItems/DestructibleItemBase.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using NaughtyAttributes;
 
 public class DestructibleItemBase : MonoBehaviour
 {
-    public HealthBase healthBase;
-    public float shakeDuration = .1f;
-    public int shakeForce = 5;
+    [SerializeField, BoxGroup("References")] public HealthBase healthBase;
+    [SerializeField, BoxGroup("References")] public GameObject coinPrefab;
+    [SerializeField, BoxGroup("References")] public Transform dropPosition;
+
+    [SerializeField, BoxGroup("Animation setup")] public float shakeDuration = .1f;
+    [SerializeField, BoxGroup("Animation setup")] public int shakeForce = 5;
+
+    [SerializeField, BoxGroup("Drop setup")] public int dropCoinsAmount = 2;
 
     private void OnValidate()
     {
@@ -23,5 +29,29 @@ public class DestructibleItemBase : MonoBehaviour
     private void OnDamage(HealthBase h)
     {
         transform.DOShakeScale(shakeDuration, Vector3.up, shakeForce);
+        DropCoins();
+    }
+
+    [Button]
+    private void DropCoins()
+    {
+        var coin = Instantiate(coinPrefab);
+        coin.transform.position = dropPosition.position;
+        coin.transform.DOScale(0, 1f).SetEase(Ease.OutBack).From();
+    }
+
+    [Button]
+    private void DropGroupOfCoins()
+    {
+        StartCoroutine(DropGroupOfCoinsCoroutine());
+    }
+
+    IEnumerator DropGroupOfCoinsCoroutine()
+    {
+        for (int i = 0; i < dropCoinsAmount; i++)
+        {
+            DropCoins();
+            yield return new WaitForSeconds(.1f);
+        }
     }
 }
